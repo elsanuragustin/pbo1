d568a445544e132680ce9cd1fb95692e1df340ff
elsanuragustin <93563142+elsanuragustin@users.noreply.github.com>
2022-02-08 12:53:27 +0700

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+
+  mounted(){
+    this.getData()
+  },
+  
+  methods:{
+    async getData(){
+      let {data,salah}= await this.$supabase
+      .from('tb_produk')
+      .select()
+
+      if(data) console.log(data)
+      if(salah) console.error(salah)
+    },
+  },
+};
+</script>
---
+++ src/views/Products.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>products</h1>
+            <ul>v-for=</ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                products: ''
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
